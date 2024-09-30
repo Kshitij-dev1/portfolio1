59e403e4c5969100e85a6ceb69800618a0e104c3
Kshitij-dev1 <Kshitij95official@gmail.com>
2024-09-30 16:19:33 +0530

    make code clean and readable

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ window.onscroll = function(){
         nav.classList.remove("header_scrolled");
     }
 }
+
+
 // nav hide
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
@@ -17,3 +19,23 @@ navBar.forEach(function(a){
         navCollapse.classList.remove("show");
     })
 })
+
+
+// <!-- Contect Form script -->
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz2JdIvWRbQWIJRE6p1_7bTspr-nNBzR2anuwzOne8lzVBSmQaNYnxFH6f0zlSdLPm78Q/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message Sent Successfully &#9996"
+            setTimeout(function(){
+                msg.innerHTML =""
+            },3000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
